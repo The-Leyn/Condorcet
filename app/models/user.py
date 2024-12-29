da62cf06a0a48379bab670683ba86d1ca06414bf
christopher DE PASQUAL <eddyjeanchristopher10@gmail.com>
2024-12-29 21:56:02 +0100

    Mise à jour du fichier user.py pour la page de connexion

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,6 @@
 from flask import current_app
 from bson import ObjectId
+import hashlib
 
 class UserModel:
     @staticmethod
@@ -9,6 +10,7 @@ class UserModel:
         user = current_app.db.users.find_one({"_id": object_id})
         return user
     
+    @staticmethod
     def find_all_user():
         """Trouver tout les utilisateurs."""
         users = list(current_app.db.users.find())
@@ -19,3 +21,31 @@ class UserModel:
         """Créer un nouvel utilisateur."""
         result = current_app.db.users.insert_one(data)
         return result.inserted_id
+    
+    @staticmethod
+    def find_by_email(email):
+        """Trouver un utilisateur par son addresse email"""
+        user = current_app.db.users.find_one({"email": email})
+        return user
+    
+    @staticmethod
+    def verify_password(input_password, stored_password_hash):
+        """Vérifie si le mot de passe correspond au hachage."""
+        hashed_input = hashlib.sha256(input_password.encode()).hexdigest()
+        return hashed_input == stored_password_hash
+    
+    @staticmethod
+    def login(email, password):
+        """
+        Connecte un utilisateur via son email et mot de passe.
+        Retourne l'utilisateur si les identifiants sont corrects.
+        """
+        user = UserModel.find_by_email(email)
+        if not user:
+            return {"error": "Utilisateur introuvable."}, False
+
+        # Vérification du mot de passe
+        if UserModel.verify_password(password, user["password_hash"]):
+            return {"message": "Connexion réussie.", "user": user}, True
+        else:
+            return {"error": "Mot de passe incorrect."}, False
