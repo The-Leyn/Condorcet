91c3f1816bb2f9a845e835d4853fd81e8a450544
Amaury057 <amaury.sanchez057@gmail.com>
2025-01-07 16:51:33 +0100

    inscription

--- app/models/user.py
+++ app/models/user.py
@@ -2,6 +2,7 @@ from flask import current_app
 from flask import session
 from bson import ObjectId
 from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
 
 class UserModel:
     @staticmethod
@@ -54,3 +55,22 @@ class UserModel:
         """Déconnecte l'utilisateur."""
         session.clear()
         return {"message": "Déconnexion réussie."}
+    
+#INSCRIPTION    
+    @staticmethod
+    def register(user_data):
+        email = user_data["email"]
+        pseudonym = user_data["pseudonym"]
+
+        # Vérifie si un utilisateur existe
+        existing_user = current_app.db.users.find_one({
+            "$or": [{"email": email}, {"pseudonym": pseudonym}]
+        })
+
+        if existing_user:
+            raise ValueError("L'email ou le pseudonyme est déjà utilisé.")
+
+        # Hache le mot de passe
+        user_data["password_hash"] = generate_password_hash(user_data["password_hash"])
+        result = current_app.db.users.insert_one(user_data)
+        return result.inserted_id
--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,6 +1,7 @@
 # app/routes/user_routes.py
 from flask import Blueprint, jsonify, render_template, request, jsonify, redirect, url_for, session
 from app.models.user import UserModel
+from werkzeug.security import generate_password_hash
 import logging
 
 # Définir le blueprint
@@ -55,3 +56,49 @@ def logout():
     else:
         UserModel.logout()
         return redirect(url_for('main.home'))
+    
+
+@user_routes.route("/register", methods=["GET", "POST"])
+def register():
+    
+    if 'user_id' in session:
+        return redirect(url_for('main.home'))
+    
+    if request.method == "GET":
+        return render_template("register.html")
+    
+    if request.method == "POST":
+
+        email = request.form.get('email')
+        pseudonym = request.form.get('pseudonym')
+        firstname = request.form.get('firstname')
+        lastname = request.form.get('lastname')
+        password = request.form.get('password')
+        repeatpassword = request.form.get('repeatpassword')
+
+        if not email or not password or not pseudonym or not firstname or not lastname:
+            error_message = "Veuillez remplir tous les champs."
+            return render_template("register.html", error=error_message)
+
+        if password != repeatpassword:
+            error_message = "Les mots de passe ne correspondent pas."
+            return render_template("register.html", error=error_message)
+        
+        # création de l'utilisateur
+        try:
+            user_data = {
+                "pseudonym": pseudonym,
+                "firstname": firstname,
+                "lastname": lastname,
+                "email": email,
+                "password_hash": password,
+                "is_active": True,
+                "role": "user",
+                "scrutin": [],
+            }
+            UserModel.register(user_data)
+            return redirect(url_for('main.home'))
+        except ValueError as e:
+            error_message = str(e)
+            return render_template("register.html", error=error_message)
+            
