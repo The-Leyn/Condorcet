207d3c57e5c0966bd50b3b7917c8c5ff4bfe7cea
Adrien Allard <allard15@hotmail.fr>
2025-01-11 16:11:20 +0100

    Update of find_10_last_active() method in Scrutin model so it match the request

--- app/models/scrutin.py
+++ app/models/scrutin.py
@@ -27,28 +27,28 @@ class ScrutinModel:
     
     @staticmethod
     def find_10_last_active():
-        """Récupérer les 10 derniers scrutins actif."""
-        now = datetime.today()
+        """Récupérer les 10 derniers scrutins actifs distincts (avec des votes récents)."""
         scrutins = list(
             current_app.db.users.aggregate([
-                {"$unwind": "$scrutin"},  # $unwind Permet de transformer chaque élément du tableau scrutin en un document distinct. Donc chaque scrutin est traité individuellement.
-                {"$match": {"scrutin.end_date": {"$lt": now}}},
-                {"$sort": {"scrutin.created_at": -1 }},  # Trier par date de création décroissante
-                {"$limit": 10},  # Limiter à 10 résultats
-                {"$project": {  # Sélectionner uniquement les champs nécessaires
-                    "_id": 0,
-                    "scrutin_id": "$scrutin.scrutin_id",
-                    "title": "$scrutin.title",
-                    "description": "$scrutin.description",
-                    "created_at": "$scrutin.created_at",
-                    "start_date": "$scrutin.start_date",
-                    "end_date": "$scrutin.end_date",
-                    "options": "$scrutin.options",
-                    "votes": "$scrutin.votes"
-                }}
+                {"$unwind": "$scrutin"},
+                {"$unwind": "$scrutin.votes"},
+                {"$sort": {"scrutin.votes.created_at": -1}},
+                {"$group": {  # Grouper par scrutin_id pour obtenir des scrutins uniques
+                    "_id": "$scrutin.scrutin_id",  # Grouper par identifiant de scrutin
+                    "title": {"$first": "$scrutin.title"},
+                    "description": {"$first": "$scrutin.description"},
+                    "created_at": {"$first": "$scrutin.created_at"},
+                    "start_date": {"$first": "$scrutin.start_date"},
+                    "end_date": {"$first": "$scrutin.end_date"},
+                    "options": {"$first": "$scrutin.options"},
+                    "last_vote_date": {"$max": "$scrutin.votes.created_at"},  # Dernière date de vote
+                }},
+                {"$sort": {"last_vote_date": -1}},  # Trier par date du dernier vote
+                {"$limit": 10}
             ])
         )
         return scrutins
+
     
     @staticmethod
     def find_created_by_user(user_id):
